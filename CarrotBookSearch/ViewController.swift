c8ac3c16bbe52a6ff7c1cf6fb0edaf5de656a854
이상호 <tapi@isanghoui-MacBookAir.local>
2026-01-07 20:21:47 +0900

    Initialize project
    xcworkspace 생성 및 프로젝트 traking

---
+++ CarrotBookSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CarrotBookSearch
+//
+//  Created by 이상호 on 1/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CarrotBookSearchTests/CarrotBookSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarrotBookSearchTests.swift
+//  CarrotBookSearchTests
+//
+//  Created by 이상호 on 1/7/26.
+//
+
+import Testing
+@testable import CarrotBookSearch
+
+struct CarrotBookSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
