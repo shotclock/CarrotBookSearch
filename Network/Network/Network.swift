006762d18917e4811553b462710548b146e954dc
이상호 <tapi@isanghoui-MacBookAir.local>
2026-01-07 21:11:04 +0900

    #1 Create Network module
    Network / NetworkInterface 모듈 생성

---
+++ Network/Network/Network.swift
@@ -0,0 +1,9 @@
+//
+//  Network.swift
+//  Network
+//
+//  Created by 이상호 on 1/7/26.
+//
+
+import Foundation
+
---
+++ Network/NetworkTests/NetworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTests.swift
+//  NetworkTests
+//
+//  Created by 이상호 on 1/7/26.
+//
+
+import Testing
+@testable import Network
+
+struct NetworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
