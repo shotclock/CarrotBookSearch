5aa2ca87dc56ad0c2815050722288d2dab12b7b9
이상호 <tapi@isanghoui-MacBookAir.local>
2026-01-10 00:07:00 +0900

    #14 Configure BookList search field
    검색 필드 UI 구성

--- List/List/Source/BookList/BookListViewController.swift
+++ List/List/Source/BookList/BookListViewController.swift
@@ -10,14 +10,47 @@ import Base
 
 // 뷰 컨트롤러 -> 뷰모델
 protocol BookListViewControllerListener: AnyObject {
-    
+    func didRequestSearch(keyword: String)
 }
 
-final class BookListViewController: UIViewController, BookListViewControllable, BookListViewControllerPresentable {
+final class BookListViewController: UIViewController,
+                                    BookListViewControllable,
+                                    BookListViewControllerPresentable {
     // MARK: Definition
+    struct UI {
+        struct LogoImageView {
+            static let topPadding: CGFloat = 10
+            static let size: CGSize = .init(width: 100,
+                                            height: 100)
+        }
+        
+        struct SearchTextField {
+            static let topSpacing: CGFloat = 10
+            static let horizontalPadding: CGFloat = 15
+        }
+    }
     
     // MARK: Properties
     weak var listener: BookListViewControllerListener?
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = .init(resource: .logo)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
+    
+    private let searchTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.placeholder = "검색어를 입력하세요 !"
+        textField.clearButtonMode = .whileEditing
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        return textField
+    }()
 
     // MARK: initializer
     deinit {
@@ -33,7 +66,32 @@ final class BookListViewController: UIViewController, BookListViewControllable, 
     
     // MARK: UI setting
     private func setupUI() {
+        view.backgroundColor = .white
         
+        setupLogoImageView()
+        setupSearchTextField()
+    }
+    
+    private func setupLogoImageView() {
+        view.addSubview(logoImageView)
+        
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: UI.LogoImageView.topPadding),
+            logoImageView.widthAnchor.constraint(equalToConstant: UI.LogoImageView.size.width),
+            logoImageView.heightAnchor.constraint(equalToConstant: UI.LogoImageView.size.height)
+        ])
+    }
+    
+    private func setupSearchTextField() {
+        view.addSubview(searchTextField)
+        searchTextField.delegate = self
+        
+        NSLayoutConstraint.activate([
+            searchTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: UI.SearchTextField.topSpacing),
+            searchTextField.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: UI.SearchTextField.horizontalPadding),
+            searchTextField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -UI.SearchTextField.horizontalPadding)
+        ])
     }
     
     // MARK: Public methods
@@ -41,3 +99,11 @@ final class BookListViewController: UIViewController, BookListViewControllable, 
     // MARK: Private methods
 }
 
+extension BookListViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        let keyword = textField.text ?? ""
+        listener?.didRequestSearch(keyword: keyword)
+        
+        return true
+    }
+}
