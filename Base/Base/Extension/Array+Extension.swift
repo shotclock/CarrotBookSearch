d361702139bdbf64878a76df625b66717d000389
이상호 <tapi@isanghoui-MacBookAir.local>
2026-01-10 01:09:09 +0900

    #14 Implement safe subscript to prevent array out-of-bounds
    배열 인덱스 초과 에러를 방지하기 위한 safe subscript 추가

---
+++ Base/Base/Extension/Array+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Array+Extension.swift
+//  Base
+//
+//  Created by 이상호 on 1/10/26.
+//
+
+public extension Array {
+    subscript(safe index: Int) -> Element? {
+        get { indices.contains(index) ? self[index] : nil }
+        set {
+            guard let newValue = newValue, indices.contains(index) else { return }
+            self[index] = newValue
+        }
+    }
+}
