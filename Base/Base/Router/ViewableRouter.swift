1f2746ebdd0e38db9e5a12e9a5fd84ca99e8aa41
이상호 <tapi@isanghoui-MacBookAir.local>
2026-01-10 15:54:23 +0900

    #7 Fix typo
    interactor 스펠링 틀린 부분 수정

--- Base/Base/Router/ViewableRouter.swift
+++ Base/Base/Router/ViewableRouter.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 open class ViewableRouter<InteractorType, ViewControllable>: ViewableRoutable {
-    public var intereactorType: InteractorType
+    public var interactorType: InteractorType
     public var navigationController: UINavigationController?
     public var childRouters: [any Routable] = []
     
     public var interactor: Interactable? {
-        intereactorType as? Interactable
+        interactorType as? Interactable
     }
     
     public var viewController: UIViewController
@@ -26,7 +26,7 @@ open class ViewableRouter<InteractorType, ViewControllable>: ViewableRoutable {
                 navigationController: UINavigationController? = nil) {
         self.viewController = viewControllable
         self.navigationController = navigationController
-        self.intereactorType = interactor
+        self.interactorType = interactor
     }
     
     deinit {
