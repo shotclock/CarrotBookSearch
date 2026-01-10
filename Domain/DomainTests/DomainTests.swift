bc2e58d8d47e8010e83aec5221e9047b3f94a44f
이상호 <tapi@isanghoui-MacBookAir.local>
2026-01-10 16:20:22 +0900

    #17 Create Domain module
    도메인 모듈 생성
    인터페이스 모듈과 구현부 모듈로 분리

---
+++ Domain/DomainTests/DomainTests.swift
@@ -0,0 +1,17 @@
+//
+//  DomainTests.swift
+//  DomainTests
+//
+//  Created by 이상호 on 1/10/26.
+//
+
+import Testing
+@testable import Domain
+
+struct DomainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
